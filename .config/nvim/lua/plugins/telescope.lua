6b704914c9c6e77e2b30cf048934e112881b0f17
Charles Bihis <charles@whoischarles.com>
2024-09-18 00:48:00 -0700

    Configure telescope plugin options

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>sf",
+        ":Telescope file_browser path=%:p:h=%:p:h<cr>",
+        desc = "Browse Files",
+      },
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>sf",
+        ":Telescope file_browser path=%:p:h=%:p:h<cr>",
+        desc = "Browse Files",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("file_browser")
+    end,
+  },
+}
